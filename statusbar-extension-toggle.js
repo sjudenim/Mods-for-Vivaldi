e958977d758a5fd0dfbf0d83fbbcfdb2366ec14a
sjudenim <sjudenim@gmx.com>
2019-04-16 15:03:54 -0400

    Updated for v2.5

--- statusbar-extension-toggle.js
+++ statusbar-extension-toggle.js
@@ -1,10 +1,20 @@
+function extStyle() {
+	const style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = `
+	#browser > div.toolbar.toolbar-droptarget.toolbar-statusbar.toolbar-medium > div.toolbar.toolbar-mainbar.toolbar-extensions.toolbar-large > div.button-narrow.button-toolbar svg path {
+		d: path('M12 6v2h2V6zm0 6v2h2v-2zm0 6v2h2v-2z');
+	}
+`;};
+
 setTimeout(function wait() {
 	const extWrapper = document.querySelector('.toolbar.toolbar-mainbar.toolbar-extensions.toolbar-large');
 	const statusBar = document.querySelector('.toolbar-statusbar');
 	if (extWrapper != null) {
 		statusBar.appendChild(extWrapper);
+		extStyle();
 	}
 	else {
-		setTimeout(wait, 600);
+		setTimeout(wait, 800);
 	}
-}, 600);
+}, 800);
