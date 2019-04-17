5902e9f6e39726d5b72c3cdb0c634f7510ee243a
sjudenim <sjudenim@gmx.com>
2019-04-17 19:16:57 -0400

    Update addressfield-reload.js

--- addressfield-reload.js
+++ addressfield-reload.js
@@ -1,8 +1,29 @@
+function reloadStyle() {
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = `
+    .toolbar .button-toolbar.reload {
+        order: 8;
+        fill: var(--colorFgFaded) !important;
+        margin-top: -4px
+    }
+      
+      .toolbar .button-toolbar.reload svg {
+        flex: 0 0 20px !important;
+        width: 20px !important;
+        height: 20px !important
+    }
+      
+    `;
+    document.getElementsByTagName('head')[0].appendChild(style);
+};
+
 setTimeout(function wait() {
     const adrField = document.querySelector('.addressfield');
     const reload = document.querySelector('.button-toolbar.reload');
     if (reload != null) {
         adrField.appendChild(reload);
+        reloadStyle();
     } else {
         setTimeout(wait, 300);
     }
