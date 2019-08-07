48d47effd297fb7a9ea53e3280f69a9de28f068c
sjudenim <sjudenim@gmx.com>
2019-08-07 15:53:50 -0400

    Updated for v.2.6

--- addressbar-menu.js
+++ addressbar-menu.js
@@ -5,32 +5,24 @@ function menuStyle() {
 	.vivaldi {
         top: 0;
         left: 0;
-        z-index: 9999
-    }
-    #browser .vivaldi span.burger-icon svg path {
-        d: path('M0 2.2000004v1h18v-1zm0 6v1h18v-1zM0 14.2v1h18v-1z')
-    }
-    #browser .vivaldi span.vivaldi-v,
-    #browser .vivaldi span.burger-icon {
         height: 34px !important;
-        width: 44px !important;
-        padding: 6px 2px 0 4px !important;
+        width: 40px !important;
         fill: var(--colorFg) !important;
         opacity: 1 !important
+        z-index: 9999
     }
-    #browser .vivaldi span.vivaldi-v {
-        padding: 7px 0 0 !important
+    #browser .vivaldi span.burger-icon svg path {d: path('M0 2.2000004v1h18v-1zm0 6v1h18v-1zM0 14.2v1h18v-1z')
     }
-    #browser .vivaldi:hover span.vivaldi-v,
-    #browser .vivaldi:active span.vivaldi-v,
-    #browser .vivaldi:hover span.burger-icon,
-    #browser .vivaldi:active span.burger-icon {
+    #browser .vivaldi span.burger-icon {
+        padding: 6px 2px 0 4px !important;
+    }
+    #browser .vivaldi:hover,
+    #browser .vivaldi:active {
         background-color: var(--colorHighlightBg) !important;
         fill: white !important
     }
       
-    #browser .vivaldi:hover span svg,
-    #browser .vivaldi:focus span svg {
+    #browser .vivaldi:hover span svg {
         transform: scale(1) !important
     }
       
