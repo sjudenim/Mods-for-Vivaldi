7837674025bf6544399e48e4760fd082c3f68c38
sjudenim <sjudenim@outlook.com>
2019-12-20 12:57:21 -0500

    Updated v.2.10

--- addressbar-menu.js
+++ addressbar-menu.js
@@ -8,13 +8,14 @@ function menuStyle() {
         height: 34px !important;
         width: 40px !important;
         fill: var(--colorFg) !important;
-        opacity: 1 !important
+        opacity: 1 !important;
         z-index: 9999
     }
-    #browser .vivaldi span.burger-icon svg path {d: path('M0 2.2000004v1h18v-1zm0 6v1h18v-1zM0 14.2v1h18v-1z')
+    #browser .vivaldi span.burger-icon svg path {
+        d: path('M0 2.2000004v1h18v-1zm0 6v1h18v-1zM0 14.2v1h18v-1z')
     }
     #browser .vivaldi span.burger-icon {
-        padding: 6px 2px 0 4px !important;
+        padding: 3px 2px 0 3px !important
     }
     #browser .vivaldi:hover,
     #browser .vivaldi:active {
