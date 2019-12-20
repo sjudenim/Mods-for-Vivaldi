bd51347db25df842c14c169ceaa0cfbdb0fa671f
sjudenim <sjudenim@outlook.com>
2019-12-20 12:49:49 -0500

    Updated for v.2.10

--- addressbar-statusbar-toggle.js
+++ addressbar-statusbar-toggle.js
@@ -2,30 +2,16 @@
 
     function statusToggle() {
         const statusContainer = document.getElementById('statusContainer');
-        const statusBar = document.querySelector('.toolbar-statusbar');
         if (statusContainer.style.display === 'block') {
             statusContainer.style.display = 'none';
-            statusBar.classList.remove('zeig');
+            statusButton.classList.remove('active');
         }
         else {
-            statusBar.classList.add('zeig');
+            statusButton.classList.add('active');
             statusContainer.style.display = 'block';
         }
     };
-    
-    function statusInfoLogic() {
-        const statusInfoToggle = document.getElementById('statusInfoToggle');
-        const statusInfo = document.querySelector('.status-info');
-        if (statusInfoToggle.classList.contains('zeig')) {
-            statusInfoToggle.classList.remove('zeig');
-            statusInfo.removeAttribute('id');
-        }
-        else {
-            statusInfoToggle.classList.add('zeig');
-            statusInfo.id = 'zeig';
-        }
-    };
-    
+
     function statusStyle() {
         const style = document.createElement('style');
         style.type = 'text/css';
@@ -36,66 +22,60 @@
         }
         #statusToggle {
             position: absolute;
-            right: calc(50vw + 22.5vw + 1px);
+            right: calc(50vw + 22.5vw - 1px);
             margin-top: -17px;
-            opacity: .4;
+            opacity: .9;
         }
-        #statusToggle:hover,
-        #statusToggle.active:hover {
+        #statusToggle:hover {
             background-color: var(--colorAccentBgFadedMore);
             fill: var(--colorAccentFg);
-            opacity: .7;
         }
-        #statusToggle.active {
-            opacity: 1
+        #statusButton.active {
+            fill: var(--colorHighlightBg);
+            opacity: 1 !important;
         }
         #statusContainer {
             position: absolute;
             z-index: 1;
-            left: calc(50vw - 22.5vw);
+            right: calc(50vw + 22.5vw + 30px);
+            top: 0;
         }
-        #statusContainer > div > div.button-toolbar:hover, #statusInfoToggle:hover, #statusInfoToggle.zeig button svg {
+        .toolbar-statusbar .button-toolbar > button:hover, .toolbar-statusbar .button-toolbar > button.button-pressed {
+            background-color: transparent !important;
             fill: var(--colorHighlightBg); 
         }
         .toolbar-statusbar {
-            background-color: var(--colorBgDarker) !important;
-            padding: 0 4px;
-            height: 30px !important;
             border: none;
-            border-radius: var(--radius);
+            background-color: transparent !important;
+            padding: 0 4px;
+            height: 34px !important;
+            border-radius: 4px;
         }
-        .toolbar-statusbar .button-popup.button-popup-above {
+        .button-popup.button-popup-above:after {
+            border-top-color: var(--colorBgDark);
+        }
+        .button-popup {
             bottom: unset;
-            top: calc(var(--toolbarHeight) + 5px) !important;
+            top: 32px;
             border-radius: 3px;
         }
-        .toolbar-statusbar .button-popup.button-popup-above:before, .toolbar-statusbar .button-popup.button-popup-above:after {
-            top: -20px;
+        .button-popup:before, .toolbar-statusbar .button-popup:after {
+            top: -10px;
             bottom: unset;
             transform: rotate(180deg)
+        }
+        .button-popup-arrow--light--above:after {
+            border-top-color: var(--colorBgDark);
+            border-top: 10px solid var(--colorBgDark);
+        }
+        .button-popup h2 {
+            background-color: var(--colorBgDark);
         }
         .status-info {
             display: none;
         }
-        #zeig.status-info.visible {
-            display: inline-block;
-            order: -1
-        }
-        #statusContainer > div > div.toolbar.toolbar-mainbar.toolbar-extensions.toolbar-large > div.button-narrow.button-toolbar svg path {d: path('M12 6v2h2V6zm0 6v2h2v-2zm0 6v2h2v-2z')}
     `;
         document.getElementsByTagName('head')[0].appendChild(style);
-    };
-    
-    function statusMod() {
-        const statusBar = document.querySelector('.toolbar-statusbar');
-        const statusInfo = document.querySelector('.status-info');
-        const divL = document.createElement('divL');
-        divL.classList.add('button-toolbar');
-        divL.id = 'statusInfoToggle';
-        divL.setAttribute('title', 'Toggle status info');
-        divL.innerHTML = '<button draggable="false" tabindex="-1"><svg width="16" height="16" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><path d="M4.53 13.649a5.388 5.388 0 0 0-1.526 3.958 5.417 5.417 0 0 0 1.808 3.847l.066.06a5.746 5.746 0 0 0 4.082 1.482 5.721 5.721 0 0 0 3.962-1.752l3.229-3.361a.499.499 0 0 0-.027-.722l-2.164-1.958-1.781 1.854.602.545-1.806 1.88c-1.141 1.188-3.057 1.251-4.281.145l-.065-.06a2.878 2.878 0 0 1-.15-4.154l1.807-1.879.602.546 1.782-1.855-2.166-1.961a.539.539 0 0 0-.745.025l-3.229 3.36zm5.249-5.46a.498.498 0 0 0 .027.721l2.166 1.959 1.85-1.925-.603-.545 1.805-1.878c1.142-1.188 3.06-1.251 4.282-.145l.066.06a2.876 2.876 0 0 1 .146 4.154l-1.804 1.878-.604-.545-1.85 1.926 2.167 1.96a.536.536 0 0 0 .743-.023l3.297-3.432a5.398 5.398 0 0 0-.283-7.807l-.066-.06a5.753 5.753 0 0 0-4.078-1.483 5.73 5.73 0 0 0-3.966 1.754L9.779 8.189zm-.704 7.105a1.196 1.196 0 0 0 .062 1.726c.509.46 1.305.434 1.779-.06l.538-.56 1.781-1.854 1.303-1.357 1.85-1.925.538-.559a1.196 1.196 0 0 0-.063-1.727 1.286 1.286 0 0 0-1.779.061l-.537.559-1.851 1.927-1.303 1.356-1.78 1.853-.538.56z"/></svg></button>';
-        statusBar.insertBefore(divL, statusInfo);
-        document.getElementById('statusInfoToggle').addEventListener('click', statusInfoLogic);
     };
     
     function statusDropdown() {
@@ -107,7 +87,7 @@
         btn.id = 'statusButton';
         btn.classList.add('button-toolbar');
         btn.setAttribute('title', 'Toggle status bar');
-        btn.innerHTML = '<button id="statusToggle" tabindex="-1"><svg width="26" height="26" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><path d="M12 6v2h2V6zm0 6v2h2v-2zm0 6v2h2v-2z"></path></button>';
+        btn.innerHTML = '<button id="statusToggle" tabindex="-1"><svg width="26" height="26" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><path d="M15.486 18l.514-.469L11.026 13 16 8.469 15.486 8 10 13z"></path></button>';
         adr.insertBefore(btn, document.querySelector('.toolbar-extensions'));
         const cont = document.createElement('div');
         cont.id = 'statusContainer';
