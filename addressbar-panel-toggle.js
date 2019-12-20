fd4c03dafe8cd5c788a7779bd3b528cdee3c0562
sjudenim <sjudenim@outlook.com>
2019-12-20 12:55:28 -0500

    Updated for v.2.10

--- addressbar-panel-toggle.js
+++ addressbar-panel-toggle.js
@@ -7,23 +7,46 @@
         .button-toolbar.panel-toggle {
             position: absolute;
             top: 0;
-            right: 120px;
+            right: 132px;
+            width: 44px;
             transform: none !important;
-        }
-        .button-toolbar.panel-toggle:hover svg path {d: path('M16.5 18.5v-9l-7 4.5z') !important
-        }
-        .button-toolbar.panel-toggle.active:hover svg path {d: path('M9.5 9.5v9l7-4.5z') !important
+            z-index: 88;
         }
         #main > div.toolbar.toolbar-addressbar.toolbar-mainbar.toolbar-large > div.button-toolbar.panel-toggle.paneltoggle > button:hover,
         #main > div.toolbar.toolbar-addressbar.toolbar-mainbar.toolbar-large > div.button-toolbar.panel-toggle.paneltoggle > button.active:hover {
+            background-color: var(--colorAccentBgFadedMore) !important;
             fill: var(--colorAccentFg);
-            opacity: .7 !important;
         }
         .button-toolbar.panel-toggle.active {
-            background-color: var(--colorBgDarker) !important;
             fill: var(--colorHighlightBg);
             opacity: 1 !important;
-            
+        }
+        /* Turns the overlay into a dropdown menu */
+        .button-toolbar.panel-toggle.active:before {
+            content: '';
+            border-color: hsla(0, 0%, 0%, 0) hsla(0, 0%, 0%, 0) var(--colorBgDark) hsla(0, 0%, 0%, 0);
+            border-style: solid;
+            border-width: 8px;
+            bottom: -4px;
+            height: 0;
+            right: 14px;
+            position: absolute;
+            width: 0;
+            z-index: 2;
+            opacity: .98;
+        }
+        div#main.right #panels-container.overlay {
+            box-shadow: 0 2px 6px rgba(0, 0, 0, 0.25) !important;
+            height: 80vh;
+            top: 4px;
+            right: 4px !important;
+        }
+        #switch {
+            border-radius: 0 3px 3px 0;
+        }
+        .right#panels-container.overlay .panel-group {
+            border-radius: 3px 0 0 3px;
+            backdrop-filter: blur(5px);
         }
     `;
         document.getElementsByTagName('head')[0].appendChild(style);
@@ -36,8 +59,8 @@
             let paneltog = document.querySelector(".button-toolbar.panel-toggle");
             let panelsvg = document.querySelector(".button-toolbar.panel-toggle svg");
             let panelpath = document.querySelector(".button-toolbar.panel-toggle svg path");
-            let paneltogbtn = "d: path('M16.153 9.533l-6.538 4.219c-.154.097-.154.399 0 .498l6.538 4.219a.203.203 0 0 0 .232-.01.306.306 0 0 0 .114-.245v-8.43a.31.31 0 0 0-.114-.244.205.205 0 0 0-.232-.007zm-.123 8.215l-5.809-3.75 5.809-3.744v7.494z')";
-            let paneltogbtnA = "d: path('M17.5 10.5h-9l4.5 7z')";
+            let paneltogbtn = "d: path('M19 10.513L18.438 10 13 14.973 7.563 10 7 10.513 13 16z')";
+            let paneltogbtnA = "d: path('M19 10.513L18.438 10 13 14.973 7.563 10 7 10.513 13 16z')";
             paneltog.classList.add('button-toolbar', 'paneltoggle');
             paneltog.classList.remove('button-toolbar-small');
             panelsvg.setAttributeNS(null, "viewBox", "0 0 26 26");
