50d3917a8773a4f856da45b3086fcfcbcdd32064
sjudenim <sjudenim@outlook.com>
2020-03-19 15:01:30 -0400

    Update addressbar-buttonGroup.js

--- addressbar-buttonGroup.js
+++ addressbar-buttonGroup.js
@@ -1,3 +1,11 @@
+/*
+* Vivaldi Button Group
+* Written by sjudenim
+* GNU General Public License v3.0
+* 
+*  Moves the button group (minimize, restore, close) to the end of addressbar
+*/
+
 setTimeout(function wait() {
     const adrBar = document.querySelector('.toolbar.toolbar-addressbar');
     const windowButtons = document.querySelector('.window-buttongroup');
