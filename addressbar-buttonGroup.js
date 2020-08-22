230e66d4a245d3c7d7f50ab54bf01cf65d1d8aa1
sjudenim <sjudenim@outlook.com>
2020-08-22 18:09:40 -0400

    Updated for v.3.3

--- addressbar-buttonGroup.js
+++ addressbar-buttonGroup.js
@@ -7,7 +7,7 @@
 */
 
 setTimeout(function wait() {
-    const adrBar = document.querySelector('.toolbar.toolbar-addressbar');
+    const adrBar = document.querySelector('.UrlBar');
     const windowButtons = document.querySelector('.window-buttongroup');
     if (windowButtons) {
         adrBar.appendChild(windowButtons);
