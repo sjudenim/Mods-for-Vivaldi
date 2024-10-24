3721f9cb0cafe1cd57d630f5db13939bd1c2e21a
sjudenim <sjudenim@outlook.com>
2024-10-24 09:51:42 -0400

    Updated for v.7.0

--- addressfield-reload.js
+++ addressfield-reload.js
@@ -1,39 +1,36 @@
-//
-//  Vivaldi Addressfield Reload (updated for browser version 3.6)
-//  Written by nomadic and sjudenim
-//  GNU General Public License v3.0
-//
-//  Moves the reload button to the end of the addressfield and replaces the icon
-//
+/*
+*  Vivaldi Addressfield Reload (updated for browser version 7.0)
+*  Written by nomadic and sudenim
+*  GNU General Public License v3.0
+*
+*  Moves the reload button to the end of the addressfield
+*/
 
 (function () {
     function moveReloadButton() {
       function style() {
         const style = document.createElement("style");
         style.innerHTML = `
-        #reload { order: 1 }
-        #reload svg { flex: 0 0 22px !important; width: 22px !important; height: 22px !important; margin-left: 4px; margin-top: 8px; }
-        .button-toolbar > button[title="Reload current page"] svg path, .button-toolbar[title="Reload current page"] svg path { d: path('M14.997 6.063L15 1.409l-1.854 1.852-.203-.211A6.953 6.953 0 007.997 1C4.142 1 1 4.143 1 8c0 3.859 3.142 7 6.997 7 3.858 0 6.997-3.141 6.997-7h-1.355a5.647 5.647 0 01-5.642 5.645A5.648 5.648 0 012.355 8a5.647 5.647 0 015.642-5.641 5.6 5.6 0 013.989 1.65l.203.208-1.844 1.847 4.652-.001z') }
-        .button-toolbar > button[title="Stop"] svg path, .button-toolbar[title="Stop"] svg path { d: path('M14 2.929L13.07 2 8 7.071 2.929 2 2 2.929 7.071 8 2 13.07l.929.93L8 8.93 13.07 14l.93-.932L8.932 8z') }
+        #reload { height: 20px; width: 20px; }
+        #reload svg { flex: 0 0 22px !important; width: 22px !important; height: 22px !important; }
      `;
         document.getElementsByTagName("head")[0].appendChild(style);
       }
   
       function reload() {
-        let urlField = document.querySelector(".toolbar-small.toolbar-insideinput");
+        let urlField = document.querySelector(".UrlBar-AddressField");
   
         let reloadButton =
-          document.querySelector('.button-toolbar[title="Reload current page"]') ||
+          document.querySelector('.button-toolbar[title="Reload (Ctrl+R)"]') ||
           document.querySelector('.button-toolbar[title="Stop"]');
         // noticed a difference in HTML structure between 2 installs, this fixes that
         if (!reloadButton) {
           reloadButton =
-            document.querySelector('.button-toolbar > button[title="Reload current page"]') ||
+            document.querySelector('.button-toolbar > button[title="Reload (Ctrl+R)"]') ||
             document.querySelector('.button-toolbar > button[title="Stop"]');
           reloadButton = reloadButton.parentElement;
         }
   
-        // Add an ID `#reload` so Tam's mod isn't necessary.
         // Chose ID over Class, use the commented out line if you want to use Class instead
         // reloadButton.classList.add("reload");
         reloadButton.id = "reload";
@@ -80,5 +77,5 @@
         clearInterval(intervalID);
         moveReloadButton();
       }
-    }, 300);
+    }, 600);
   })();
