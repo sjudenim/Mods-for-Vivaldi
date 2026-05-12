02a5f664390d076871845c5bfad53b50823fab61
sjudenim <sjudenim@outlook.com>
2026-05-12 16:40:10 -0400

    Switched to full svg icons

--- UI/JS/vivaldi-mini-player.js
+++ UI/JS/vivaldi-mini-player.js
@@ -3,7 +3,7 @@
  * For Vivaldi browser version 7.9 and up
  * Author: sudenim and Tam710562
  *
- * Description: Adds a draggable mini player to a page when the media source is a background tab
+ * Description: Adds a draggable mini player to a page when the media source is on a background tab
  *
  * GNU General Public License v3.0
  */
@@ -25,12 +25,17 @@
   let sliderThumb = null;
   let sliderDragging = false;
 
-  // VOLUME ICONS
+  // SVG ICONS
   // --------------------------------------------------
   const volumeIcons = {
     high: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"/></svg>`,
     medium: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M5,9V15H9L14,20V4L9,9M18.5,12C18.5,10.23 17.5,8.71 16,7.97V16C17.5,15.29 18.5,13.76 18.5,12Z"/></svg>`,
     off: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M12,4L9.91,6.09L12,8.18M4.27,3L3,4.27L7.73,9H3V15H7L12,20V13.27L16.25,17.53C15.58,18.04 14.83,18.46 14,18.7V20.77C15.38,20.45 16.63,19.82 17.68,18.96L19.73,21L21,19.73L12,10.73M19,12C19,12.94 18.8,13.82 18.46,14.64L19.97,16.15C20.62,14.91 21,13.5 21,12C21,7.72 18,4.14 14,3.23V5.29C16.89,6.15 19,8.83 19,12M16.5,12C16.5,10.23 15.5,8.71 14,7.97V10.18L16.45,12.63C16.5,12.43 16.5,12.21 16.5,12Z"/></svg>`
+  };
+
+  const playerIcons = {
+    play: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M8,5.14V19.14L19,12.14L8,5.14Z"/></svg>`,
+    pause: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M14,19H18V5H14M6,19H10V5H6V19Z"/></svg>`
   };
 
   // UI LAYOUT
@@ -239,7 +244,7 @@
 
     if (paused !== lastPaused) {
       lastPaused = paused;
-      playBtn.textContent = paused ? "❚❚" : "▶";
+      playBtn.innerHTML = paused ? playerIcons.pause : playerIcons.play;
     }
 
     if (!sliderDragging && volume !== lastVolume) {
